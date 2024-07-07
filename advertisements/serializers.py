b3fc1a8fecade552bc421b07540a3ea4ad33ba4f
Vitaliy_Kanin <kaninv1611997@mail.ru>
2024-07-07 13:47:28 +0300

    Django_Permissions_07_07

--- advertisements/serializers.py
+++ advertisements/serializers.py
@@ -41,16 +41,16 @@ class AdvertisementSerializer(serializers.ModelSerializer):
         """Метод для валидации. Вызывается при создании и обновлении."""
 
         # TODO: добавьте требуемую валидацию
-        status_obj = Advertisement.objects.all()
-        flag = True
-        count = 0
         print(data)
-        for i in status_obj:
-            if i.status == "OPEN":
-                count += 1
-        if count == 10:
-            print(count)
-        elif count >= 10:
-            print("Уже есть 10 открытых объявлений, так нельзя ")
+        print(self.context["request"].user)
+        status_obj = Advertisement.objects.filter(creator=self.context["request"].user, status=data["status"]).count()
+        print(status_obj)
+        if status_obj >= 10:
+            raise serializers.ValidationError({"У пользователя не должно быть больше 10 открытых или закрытых объявлений"})
 
         return data
+
+    def delete(self):
+        print("TEST")
+        # validated_data["delete"] = self.context["request"].user
+        # return super().delete(validated_data)
